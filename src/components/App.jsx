f9d5e28b6327fce9fab4fa4aa2008d64d2306d6c
AndreyTarkaniy <endrubass@gmail.com>
2023-06-29 17:45:49 +0300

    fixx past code

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,16 @@
-import { Container } from 'components/container/container';
-import Form from 'components/form/form';
-import ContactsList from 'components/contactsList/contactsList';
-import Filter from 'components/filter/filter';
+// import { Container } from 'components/container/container';
+// import Form from 'components/form/form';
+// import ContactsList from 'components/contactsList/contactsList';
+// import Filter from 'components/filter/filter';
 import { useDispatch } from 'react-redux';
 import { getContactsThunk } from 'redux/contacts/thunkApi';
 import { useEffect } from 'react';
-import { useContacts } from 'redux/contacts/useContacts';
+// import { useContacts } from 'redux/contacts/useContacts';
 import { Route, Routes } from 'react-router-dom';
 import { Layout } from './Layout';
 
 export const App = () => {
-  const { isLoading, error } = useContacts();
+  // const { isLoading, error } = useContacts();
   const dispatch = useDispatch();
 
   useEffect(() => {
