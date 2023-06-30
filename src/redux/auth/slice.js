5a882497c8258da50ebe96ed66661787af114f2d
AndreyTarkaniy <endrubass@gmail.com>
2023-06-30 15:51:28 +0300

    add authslice and refresh store

---
+++ src/redux/auth/slice.js
@@ -0,0 +1,29 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { login, register } from './operations';
+
+const initialState = {
+  user: { name: null, email: null },
+  token: null,
+  isLoggedIn: false,
+  isRefreshing: false,
+};
+
+const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  extraReducers: builder => {
+    builder
+      .addCase(register.fulfilled, (state, { payload }) => {
+        state.user = payload.user;
+        state.token = payload.token;
+        state.isLoggedIn = true;
+      })
+      .addCase(login.fulfilled, (state, { payload }) => {
+        state.user = payload.user;
+        state.token = payload.token;
+        state.isLoggedIn = true;
+      });
+  },
+});
+
+export const authReducer = authSlice.reducer;
--- src/redux/store.js
+++ src/redux/store.js
@@ -2,7 +2,12 @@ import { configureStore } from '@reduxjs/toolkit';
 
 import { contactsReducer } from './contacts/contactsSlice';
 import { filterReducer } from './contacts/filterSlice';
+import { authReducer } from './auth/slice';
 
 export const store = configureStore({
-  reducer: { contacts: contactsReducer, filter: filterReducer },
+  reducer: {
+    contacts: contactsReducer,
+    filter: filterReducer,
+    auth: authReducer,
+  },
 });
