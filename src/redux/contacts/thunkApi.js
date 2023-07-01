7feb7cf7eb5db442cb2a3ec56554294eb934d976
AndreyTarkaniy <endrubass@gmail.com>
2023-07-01 17:48:19 +0300

    change auth.operation

--- src/redux/contacts/thunkApi.js
+++ src/redux/contacts/thunkApi.js
@@ -16,6 +16,17 @@ import axios from 'axios';
 //     }
 //   }
 // );
+// export const getContactsThunk = createAsyncThunk(
+//   'contacts/fetchAll',
+//   async (_, thunkAPI) => {
+//     try {
+//       const response = await axios.get('/contacts');
+//       return response.data;
+//     } catch (e) {
+//       return thunkAPI.rejectWithValue(e.message);
+//     }
+//   }
+// );
 export const getContactsThunk = createAsyncThunk(
   'contacts/fetchAll',
   async (_, thunkAPI) => {
